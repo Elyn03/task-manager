0fab0f08d11dd12bd55459f4ecf14236cd2915fb
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-07-31 23:14:30 +0200

    :adhesive_bandage: FIX outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,11 @@
 output "cloudfront_distribution_domain_name" {
   value = module.cdn.cloudfront_distribution_domain_name
 }
+
+output "cloudfront_distribution_id" {
+  value = module.cdn.cloudfront_distribution_id
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.main.bucket
+}
